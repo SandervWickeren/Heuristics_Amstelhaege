97835ed17d7ac017d265bdd2f671ffe5272e2373
Sander van Wickeren <sandervanwickeren@gmail.com>
2017-11-04 18:02:20 +0100

    Reworked from Pygame to Matplotlib Now possible to generate houses using different sizes on random locations. "ll always produce all houses on the map completely.

--- Main.py
+++ Main.py
@@ -11,63 +11,152 @@ def main():
 	"""
 	explain
 	"""
-	def GenHouse(surface, fill_color, border_color, rect, border=1):
-		surface.fill(border_color, rect)
-		surface.fill(fill_color, rect.inflate(-border*2, -border*2))
 
-	def Draw100():
-		for i in range(20):
-			y = random.randint(0, 180 * TileSize)
-			x = random.randint(0, 160 * TileSize)
-
-			GenHouse(gameDisplay, red, black, pygame.Rect(x, y, 32, 32), 8)
-
-	# Define colors.
-	white = (255,255,255)
-	black = (0,0,0)
-	red = (255, 0, 0)
-
-	# Size of the tiles in pixels.
-	TileSize = 4
-
-	# Setting up resolutions.
-	gameDisplay = pygame.display.set_mode((180 * TileSize,160 * TileSize))
-	pygame.display.set_caption('Amstelhaege')
-
-	gameRun = True
-	a = True
-
-	gameDisplay.fill(white)
-	if a == True: 
-		Draw100()
-	pygame.display.update()
-	a = False
-
-	while gameRun:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				gameRun = False
-
-
-	# Functies die geschreven kunne worden
 	def genMap(length, width):
-		return 0
+		"""
+		Input is a distance in meters. Output is a list
+		of lists containing zeros. Representing the grid.
 
-	def checkValidity(map):
-		return 0
+		Example of 2x4:
+		[[0, 0, 0, 0],
+		 [0, 0, 0, 0]]
+		"""
+		# Fixes .5
+		width = 2 * width
+		length = 2 * length
 
-	def addHouse(map):
-		return 0
+		return [width * [0] for i in range(length)]
 
-	def calcScore(map):
-		return 0
+	def genY(grid):
+		"""
+		Input is a grid, output a random Y value from the grid.
+		"""
+		return random.randint(0, len(grid))
+
+	def genX(grid):
+		"""
+		Input is a grid, output a random X value from the grid.
+		"""
+		return random.randint(0, len(grid[0]))
+
+	def placeHouse(width, length, freespace, y_cor, x_cor, grid):
+		"""
+		Takes as input:
+		- Length of the house in meters
+		- Width of the house in meters
+		- The amount of freespace in meters
+		- y_cor from where it should be generated
+		- x_cor from where it should be generated
+		- The current grid
+
+		Output:
+		- Grid with the new house added.
+		"""
+		# Assign variabele:
+		newgrid = copy.deepcopy(grid)
+
+		# Remove the .5 problem
+		width = int(2 * width)
+		length = int(2 * length)
+		freespace = int(2 * freespace)
+
+		# Place house
+		for l in range(length):
+			try:
+				
+				# Internal house
+				for w in range(width):
+					newgrid[y_cor + l][x_cor + w] = 1
+
+				# Freespace
+				for f in range(1, freespace + 1):
+					
+					# At the sides
+					newgrid[y_cor + l][x_cor - f] = 5
+					newgrid[y_cor + l][x_cor + width + f - 1] = 5
+
+			except:
+				# False placement:
+				return grid
+				
+
+		# Partial freespace
+		for w in range(width):
+			try:
+				for f in range(1, freespace + 1):
+
+					# At top/bottom
+					newgrid[y_cor - f][x_cor + w] = 5
+					newgrid[y_cor + length + f - 1][x_cor + w] = 5
+
+			except:
+				return grid
+				
+		return newgrid
+
+
+	def visualizeGrid(grid):
+		"""
+		Take in a grid and outputs a mapping of
+		the grid using various colors.
+		"""
+		import matplotlib.pyplot as plt
+		from matplotlib import colors
+
+		# Define colors [Background, House1, freespace]
+		colormap = colors.ListedColormap(["#FFFFFF", "#D50000", "#FF8A80"])
 		
+		# 0-1: white, 1-5: red etc.
+		bounds = [0, 1, 5, 8]
+		norm = colors.BoundaryNorm(bounds, colormap.N)
+
+		# Make plot
+		fig, ax = plt.subplots()
+		ax.imshow(grid, cmap=colormap, norm=norm)
 
 
+		# Show plot
+		plt.show()
 
+		return 0
+
+
+	def calcScore(grid):
+		return 0
+
+
+	def test():
+		"""
+		Temporarily test function.
+		"""
+		gr = genMap(50, 50)
+
+		# Place 5 house test:
+		t = 0
+		while t != 10:
+
+			ah = placeHouse(4, 4, 1, genY(gr), genX(gr), gr)
+
+			if ah != gr:
+				gr = list(ah)
+				t += 1
+			else:
+				print "Retry."
+		
+		visualizeGrid(ah)
+		return 0
+
+	test()
+	
 
 
 if __name__ == "__main__":
-	import pygame
+	#import pygame
 	import random
+	import copy
 	main()
+
+
+	
+
+	
